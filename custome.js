5b9e16967ac60392a14333ab0b82b1211955cf3c
Ravuthz <ravuthz@gmail.com>
2015-10-23 01:23:37 +0700

    Create custome.js

---
+++ custome.js
@@ -0,0 +1,39 @@
+.blog-post-head h2 {
+  background: none rgb(255, 255, 255);
+  border-bottom-color: rgb(170, 170, 170);
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+  clear: left;
+  font-family: sans-serif;
+  font-size: 19.2px;
+  font-weight: normal;
+  line-height: 19.2px;
+  margin: 0px 0px 0.6em;
+  overflow: hidden;
+  padding-bottom: 0.17em;
+  padding-top: 0.5em;
+  width: auto;
+}
+.blog-post-list {
+  background-color: white;
+  font-family: sans-serif;
+  font-size: 12.8px;
+  line-height: 19.2px;
+  //list-style-image: url(data:image/png);
+  list-style-type: square;
+  margin: 0.3em 0px 0px 1.6em;
+  padding: 0px;
+}
+.blog-post-list li {
+  margin-bottom: 0.1em;
+}
+.blog-post-list ul {
+  line-height: 1.5em;
+  //list-style-image: url(data:image/png);
+  list-style-type: square;
+  margin: 0.3em 0px 0px 1.6em;
+  padding: 0px;
+}
+.text-khmer {
+  font-family: Suwannaphum;
+}
