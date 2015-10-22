f7bb6a94507411ec938b13123c2560f892ee6b8b
Ravuthz <ravuthz@gmail.com>
2015-10-23 02:42:12 +0700

    Create custome.js

---
+++ custome.js
@@ -0,0 +1,4 @@
+$(function(){
+  $('#carousel').html('<script src=\"/feeds/posts/default?max-results="+numposts1+"&orderby=published&alt=json-in-script&callback=showrecentposts1\"><\/script>');
+
+});
