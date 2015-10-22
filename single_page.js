48b09c3d345d5441c9a2166aba22c8e6932045cb
Ravuthz <ravuthz@gmail.com>
2015-10-23 03:02:06 +0700

    Create single_page.js

---
+++ single_page.js
@@ -0,0 +1,65 @@
+h2.post-title a, h1.post-title a, h2.post-title, h1.post-title {
+  font-family: Roboto Slab;
+  line-height: 40px;
+  font-weight: 400;
+  font-style: normal;
+  color: #333333;
+  font-size: 40px;
+  opacity: 1;
+  visibility: visible;
+  -webkit-transition: opacity 0.24s ease-in-out;
+  -moz-transition: opacity 0.24s ease-in-out;
+  transition: opacity 0.24s ease-in-out;
+}
+
+.top-comment-widget-menu {
+  float: left;
+  margin: -15px 0 15px;
+  padding: 0;
+  width: 100%;
+  height: 40px;
+  background: #444444 url(http://2.bp.blogspot.com/-YxxeRcqP6UI/U6V7A_pfmnI/AAAAAAAAAik/NyzEC4z7POQ/s1600/menu-bg2.png) repeat;
+}
+
+.top-comment {
+  float: left;
+  font-size: 14px;
+  list-style: none outside none;
+  text-transform: uppercase;
+  width: 43%;
+  margin: 1px;
+  padding: 10px 20px !important;
+  font-weight: normal;
+  color: #fff;
+  cursor: pointer;
+}
+
+#relpost_img_sum .news-text {
+  display: none;
+}
+
+.top-comment.selected {
+  cursor: pointer;
+  padding: 11px 20px !important;
+  margin: 0px 0 0 -16px;
+  color: #FFF;
+  background:#FF7A40;
+
+  -webkit-transition: all .2s ease-in-out;
+  -moz-transition: all .2s ease-in-out;
+  -o-transition: all .2s ease-in-out;
+  -ms-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+}
+
+.top-comment.blogico:before {
+  content: &quot;\f0d5&quot;;
+  font-family: fontawesome;
+  margin-right: 15px;
+}
+
+.top-comment.faceico:before {
+  content: &quot;\f09a&quot;;
+  font-family: fontawesome;
+  margin-right: 15px;
+}
