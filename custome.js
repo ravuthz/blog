b3c3c382fa5d836ccbb93746a94ebddf3725ce77
Ravuthz <ravuthz@gmail.com>
2015-10-23 15:13:13 +0700

    Update custome.js

--- custome.js
+++ custome.js
@@ -15,6 +15,18 @@ selectnav('menu-main', {
   indent: '-'
 });
 
+$(window).scroll(function() {
+  if($(this).scrollTop() > 200){
+    $('#back-to-top').fadeIn();
+  } else {
+    $('#back-to-top').fadeOut();
+  }
+});
+$('#back-to-top').hide().click(function() {
+  $('html, body').animate({scrollTop:0}, 1000);
+  return false;
+});
+
 $('.set-1').mtabs();
 $('.widget h2').wrapInner('<span/>');
 
